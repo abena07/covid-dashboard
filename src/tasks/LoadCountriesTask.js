4eb9d8bcf7b297525ca05692235b41d405e2b4f6
abena07 <62352130+abena07@users.noreply.github.com>
2021-02-04 18:48:57 +0000

    showing confirmed cases

--- src/tasks/LoadCountriesTask.js
+++ src/tasks/LoadCountriesTask.js
@@ -1,12 +1,48 @@
 import {features} from '../data/countries.json'
+import papa from "papaparse";
+
 
 class LoadCountriesTask{
-
-
+    covidUrl =
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv";
+    setState=null;
+    mapCountries= features;
 
     load=(setState)=>{
-        setState(features)
+        this.setState=setState;
+        papa.parse(this.covidUrl, {
+            download:true,
+            header: true,
+            complete:(result)=> this.#processCovidData(result.data)
+        });
+       
     }
+
+    #processCovidData = (covidCountries) => {
+        for (let i = 0; i < features.length; i++) {
+          const country = features[i];
+          //console.log(country);
+          const covidCountry = covidCountries.find(
+            (covidCountry) => country.properties.ISO_A3 === covidCountry.ISO3
+          );
+    
+          country.properties.confirmed = 0;
+          country.properties.confirmedText = 0;
+    
+          if (covidCountry != null) {
+            let confirmed = Number(covidCountry.Confirmed);
+            country.properties.confirmed = confirmed;
+            country.properties.confirmedText = 
+              confirmed
+            
+          }
+          
+        }
+    
+        this.setState(features);
+      };
+      
+      
 }
 
 
